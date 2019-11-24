a4c0a3ecb64eeb0e6f56940ec9a11ed567db75c0
DiviNeMint <asertyuk8@gmail.com>
2019-11-24 14:56:41 +0800

    Add ImageContext's Construction. Add PixelInfo for ImageHandle.

--- MenthaAssembly/Models/ImageContext.cs
+++ MenthaAssembly/Models/ImageContext.cs
@@ -3,11 +3,15 @@ using System;
 
 namespace MenthaAssembly
 {
-    public partial class ImageContext : IImageContext
+    public class ImageContext : IImageContext
     {
         public int Width { get; }
 
         public int Height { get; }
+
+        public int Stride { get; }
+
+        public int Channel { get; }
 
         public IntPtr Scan0 { get; }
 
@@ -19,35 +23,48 @@ namespace MenthaAssembly
 
         public IntPtr ScanB { get; }
 
-        public int Stride { get; }
-
         public int PixelBytes { get; }
-
-        public int Channel { get; }
 
         public ImageContext(int Width, int Height, IntPtr Scan0, int Stride, int PixelBytes)
         {
             this.Width = Width;
             this.Height = Height;
             this.Stride = Stride;
-            this.Scan0 = Scan0;
             this.PixelBytes = PixelBytes;
+            this.Scan0 = Scan0;
             this.Channel = 1;
         }
-
-        public ImageContext(int Width, int Height, IntPtr ScanR, IntPtr ScanG, IntPtr ScanB)
+        public ImageContext(int Width, int Height, IntPtr ScanR, IntPtr ScanG, IntPtr ScanB) : this(Width, Height, ScanR, ScanG, ScanB, Width)
+        {
+        }
+        public ImageContext(int Width, int Height, IntPtr ScanR, IntPtr ScanG, IntPtr ScanB, int Stride)
         {
             this.Width = Width;
             this.Height = Height;
-            this.Stride = Width;
+            this.Stride = Stride;
             this.ScanR = ScanR;
             this.ScanG = ScanG;
             this.ScanB = ScanB;
             this.PixelBytes = 3;
             this.Channel = 3;
         }
+        public ImageContext(int Width, int Height, IntPtr ScanA, IntPtr ScanR, IntPtr ScanG, IntPtr ScanB) : this(Width, Height, ScanA, ScanR, ScanG, ScanB, Width)
+        {
+        }
+        public ImageContext(int Width, int Height, IntPtr ScanA, IntPtr ScanR, IntPtr ScanG, IntPtr ScanB, int Stride)
+        {
+            this.Width = Width;
+            this.Height = Height;
+            this.Stride = Stride;
+            this.ScanA = ScanA;
+            this.ScanR = ScanR;
+            this.ScanG = ScanG;
+            this.ScanB = ScanB;
+            this.PixelBytes = 4;
+            this.Channel = 4;
+        }
 
-        protected byte[] Datas { set; get; } = new byte[0];
+        protected byte[][] Datas { set; get; } = new byte[4][];
         public ImageContext(int Width, int Height, byte[] Datas, int Stride, int PixelBytes)
         {
             this.Width = Width;
@@ -56,12 +73,68 @@ namespace MenthaAssembly
             this.PixelBytes = PixelBytes;
             this.Stride = Stride;
 
-            this.Datas = Datas;
+            this.Datas[0] = Datas;
             unsafe
             {
-                fixed (byte* Buffer = &Datas[0])
+                fixed (byte* Buffer = &this.Datas[0][0])
                     Scan0 = (IntPtr)Buffer;
             }
         }
+        public ImageContext(int Width, int Height, byte[] DataR, byte[] DataG, byte[] DataB) : this(Width, Height, DataR, DataG, DataB, Width)
+        {
+        }
+        public ImageContext(int Width, int Height, byte[] DataR, byte[] DataG, byte[] DataB, int Stride)
+        {
+            this.Width = Width;
+            this.Height = Height;
+            this.Channel = 3;
+            this.PixelBytes = 3;
+            this.Stride = Stride;
+
+            this.Datas[0] = DataR;
+            this.Datas[1] = DataG;
+            this.Datas[2] = DataB;
+            unsafe
+            {
+                fixed (byte* R = &this.Datas[0][0],
+                             G = &this.Datas[1][0],
+                             B = &this.Datas[2][0])
+                {
+                    ScanR = (IntPtr)R;
+                    ScanG = (IntPtr)G;
+                    ScanB = (IntPtr)B;
+                }
+            }
+        }
+        public ImageContext(int Width, int Height, byte[] DataA, byte[] DataR, byte[] DataG, byte[] DataB) : this(Width, Height, DataA, DataR, DataG, DataB, Width)
+        {
+        }
+        public ImageContext(int Width, int Height, byte[] DataA, byte[] DataR, byte[] DataG, byte[] DataB, int Stride)
+        {
+            this.Width = Width;
+            this.Height = Height;
+            this.Channel = 4;
+            this.PixelBytes = 4;
+            this.Stride = Stride;
+            
+            this.Datas[0] = DataA;
+            this.Datas[1] = DataR;
+            this.Datas[2] = DataG;
+            this.Datas[3] = DataB;
+            unsafe
+            {
+                fixed (byte* A = &this.Datas[0][0],
+                             R = &this.Datas[1][0],
+                             G = &this.Datas[2][0],
+                             B = &this.Datas[3][0])
+                {
+                    ScanA = (IntPtr)R;
+                    ScanR = (IntPtr)R;
+                    ScanG = (IntPtr)G;
+                    ScanB = (IntPtr)B;
+                }
+            }
+        }
+
     }
 }
---
+++ MenthaAssembly/Structs/PixelInfo.cs
@@ -0,0 +1,43 @@
+﻿namespace MenthaAssembly
+{
+    public struct PixelInfo
+    {
+        public static PixelInfo Empty { get; } = new PixelInfo(-1, -1, 0, 0, 0, 0);
+
+        public int X { get; }
+
+        public int Y { get; }
+
+        public byte A { get; }
+
+        public byte R { get; }
+
+        public byte G { get; }
+
+        public byte B { get; }
+
+        public bool IsEmpty
+            => X is -1 && 
+               Y is -1 && 
+               A is 0 && 
+               R is 0 && 
+               G is 0 && 
+               B is 0;
+
+        public PixelInfo(int X, int Y, int PixelData) : this(X, Y, (byte)(PixelData >> 24), (byte)(PixelData >> 16), (byte)(PixelData >> 8), (byte)PixelData)
+        {
+        }
+        public PixelInfo(int X, int Y, byte A, byte R, byte G, byte B)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.A = A;
+            this.R = R;
+            this.G = G;
+            this.B = B;
+        }
+
+        public override string ToString()
+            => $"X : {X}, Y : {Y}, A : {A}, R : {R}, G : {G}, B : {B}";
+    }
+}
