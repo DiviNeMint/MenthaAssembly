7b79f67b946d9ce796c1aebb2fd5591408a6a546
tonychen <tonychen@TWC280-TONYNB>
2022-03-02 16:50:21 +0800

    Implement DynamicLibrary.

---
+++ MenthaAssembly/Utils/Dynamics/ManagedLibrary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace MenthaAssembly
+{
+    public sealed class ManagedLibrary : DynamicLibrary
+    {
+        private readonly AppDomain Domain;
+        private readonly Assembly Assembly;
+        internal ManagedLibrary(string Path, LibraryType Type) : base(Path, Type)
+        {
+            Domain = AppDomain.CreateDomain(Guid.NewGuid().ToString());
+            //Domain.AssemblyResolve += OnDomainAssemblyResolve;
+
+            byte[] RawDatas = File.ReadAllBytes(Path);
+
+            Assembly = Domain.Load(RawDatas);
+        }
+
+        //private static Assembly OnDomainAssemblyResolve(object sender, ResolveEventArgs args) 
+        //    => throw new NotImplementedException();
+
+        public Type[] GetTypes()
+            => Assembly.GetTypes();
+
+        public override void Dispose()
+        {
+            AppDomain.Unload(Domain);
+            base.Dispose();
+        }
+    }
+
+}
+
