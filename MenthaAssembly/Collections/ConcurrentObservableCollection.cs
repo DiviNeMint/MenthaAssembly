609e1a65da3cda5865666cef1a107aa497f233f9
DiviNeMint <asertyuk8@gmail.com>
2022-08-18 00:04:39 +0800

    Collections Rename RemoveRange Func in ConcurrentObservableCollection.

--- MenthaAssembly/Collections/ConcurrentObservableCollection.cs
+++ MenthaAssembly/Collections/ConcurrentObservableCollection.cs
@@ -92,39 +92,6 @@ namespace MenthaAssembly
                     OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, RemovedItem, index));
                 }
             });
-        public virtual void RemoveRange(IEnumerable<T> Items)
-        {
-            if (Items is not T[] &&
-                Items is not IList &&
-                Items is not ICollection)
-                Items = Items.ToArray();
-
-            Handle(() =>
-            {
-                foreach (T Item in Items)
-                {
-                    if (this.Items.Remove(Item))
-                        OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, Item));
-                }
-
-                OnPropertyChanged(CountName);
-                OnPropertyChanged(IndexerName);
-            });
-
-            //CheckReentrancy();
-
-            //if (Items is not T[] &&
-            //    Items is not IList &&
-            //    Items is not ICollection)
-            //    Items = Items.ToArray();
-
-            //foreach (T Item in Items)
-            //    base.Items.Remove(Item);
-
-            //OnPropertyChanged(CountString);
-            //OnPropertyChanged(IndexerName);
-            //OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
-        }
 
         public override void Insert(int index, T item)
             => Handle(() =>
