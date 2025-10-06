6582276f6eaaa11abbaf251ffe071447d99bd624
Mentha <asertyuk8@gmail.com>
2025-10-06 17:21:13 +0800

    新增對比度和伽瑪調整的像素適配器
    這些變更在 `AdjustContrastPixelAdapter.cs` 和 `AdjustGammaPixelAdapter.cs` 檔案中新增了兩個類別，分別為 `AdjustContrastPixelAdapter<T>` 和 `AdjustGammaPixelAdapter<T>`。這兩個類別都實作了 `PixelAdapter<T>`，並且提供了對像素的對比度和伽瑪調整功能。這些類別包含了像素的屬性（如 A、R、G、B）和方法（如 Override 和 Overlay），並且使用了字典來快取調整後的像素值，以提高性能。此外，這些類別還包含了確保像素有效性的邏輯，並且在進行像素操作時會更新有效性狀態。整體來說，這些變更增強了圖像處理的功能，特別是在對比度和伽瑪調整方面。

---
+++ MenthaAssembly/Media/Imaging/PixelAdapters/AdjustGammaPixelAdapter.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MenthaAssembly.Media.Imaging.Utils
+{
+    public sealed unsafe class AdjustGammaPixelAdapter<T> : PixelAdapter<T>
+        where T : unmanaged, IPixel
+    {
+        private static readonly Type GrayType = typeof(Gray8);
+
+        private readonly IPixelAdapter Adapter;
+
+        public override int XLength { get; }
+
+        public override int YLength { get; }
+
+#pragma warning disable IDE0044 // 新增唯讀修飾元
+        // If sets readonly, it will cause pixel's value can't change.
+        private T Pixel;
+#pragma warning restore IDE0044 // 新增唯讀修飾元
+
+        public override byte A
+        {
+            get
+            {
+                EnsurePixel();
+                return Pixel.A;
+            }
+        }
+
+        public override byte R
+        {
+            get
+            {
+                EnsurePixel();
+                return Pixel.R;
+            }
+        }
+
+        public override byte G
+        {
+            get
+            {
+                EnsurePixel();
+                return Pixel.G;
+            }
+        }
+
+        public override byte B
+        {
+            get
+            {
+                EnsurePixel();
+                return Pixel.B;
+            }
+        }
+
+        public override int BitsPerPixel { get; }
+
+        public double Gamma { get; }
+
+        private readonly Action Adjuster;
+        private readonly Dictionary<byte, byte> Cache;
+        private AdjustGammaPixelAdapter(AdjustGammaPixelAdapter<T> Adapter)
+        {
+            X = Adapter.X;
+            Y = Adapter.Y;
+            XLength = Adapter.XLength;
+            YLength = Adapter.YLength;
+            BitsPerPixel = Adapter.BitsPerPixel;
+            Cache = Adapter.Cache;
+            Adjuster = Adapter.Adjuster;
+            Gamma = Adapter.Gamma;
+            this.Adapter = Adapter.Adapter.Clone();
+
+            if (Adapter.IsPixelValid)
+            {
+                IsPixelValid = true;
+                Pixel = Adapter.Pixel;
+            }
+        }
+        public AdjustGammaPixelAdapter(IPixelAdapter Adapter, double Gamma)
+        {
+            X = Adapter.X;
+            Y = Adapter.Y;
+            XLength = Adapter.XLength;
+            YLength = Adapter.YLength;
+            BitsPerPixel = Adapter.BitsPerPixel;
+            Cache = [];
+            Adjuster = typeof(T) == GrayType ? EnhanceGray : EnhanceRGB;
+            this.Gamma = Gamma;
+            this.Adapter = Adapter;
+
+            void EnhanceGray()
+            {
+                byte Gray = Adjust(Adapter.R);
+                Pixel.Override(Adapter.A, Gray, Gray, Gray);
+            }
+            void EnhanceRGB()
+            {
+                Pixel.Override(Adapter.A, Adjust(Adapter.R), Adjust(Adapter.G), Adjust(Adapter.B));
+            }
+        }
+
+        public override void Override(T Pixel)
+            => Override(Pixel.A, Pixel.R, Pixel.G, Pixel.B);
+        public override void Override(PixelAdapter<T> Adapter)
+            => Override(Adapter.A, Adapter.R, Adapter.G, Adapter.B);
+        public override void Override(byte A, byte R, byte G, byte B)
+            => Adapter.Override(A, R, G, B);
+
+        public override void OverrideTo(T* pData)
+        {
+            EnsurePixel();
+            *pData = Pixel;
+        }
+        public override void OverrideTo(byte* pDataR, byte* pDataG, byte* pDataB)
+        {
+            EnsurePixel();
+            *pDataR = Pixel.R;
+            *pDataG = Pixel.G;
+            *pDataB = Pixel.B;
+        }
+        public override void OverrideTo(byte* pDataA, byte* pDataR, byte* pDataG, byte* pDataB)
+        {
+            EnsurePixel();
+            *pDataA = Pixel.A;
+            *pDataR = Pixel.R;
+            *pDataG = Pixel.G;
+            *pDataB = Pixel.B;
+        }
+
+        public override void Overlay(T Pixel)
+            => Overlay(Pixel.A, Pixel.R, Pixel.G, Pixel.B);
+        public override void Overlay(PixelAdapter<T> Adapter)
+            => Overlay(Adapter.A, Adapter.R, Adapter.G, Adapter.B);
+        public override void Overlay(byte A, byte R, byte G, byte B)
+        {
+            if (A == byte.MaxValue)
+                Adapter.Override(A, R, G, B);
+            else
+                Adapter.Overlay(A, R, G, B);
+        }
+
+        public override void OverlayTo(T* pData)
+        {
+            EnsurePixel();
+            if (Pixel.A == byte.MaxValue)
+                *pData = Pixel;
+            else
+                pData->Overlay(Pixel.A, Pixel.R, Pixel.G, Pixel.B);
+        }
+        public override void OverlayTo(byte* pDataR, byte* pDataG, byte* pDataB)
+        {
+            EnsurePixel();
+            if (Pixel.A == byte.MaxValue)
+            {
+                *pDataR = Pixel.R;
+                *pDataG = Pixel.G;
+                *pDataB = Pixel.B;
+            }
+            else
+            {
+                PixelHelper.Overlay(ref pDataR, ref pDataG, ref pDataB, Pixel.A, Pixel.R, Pixel.G, Pixel.B);
+            }
+        }
+        public override void OverlayTo(byte* pDataA, byte* pDataR, byte* pDataG, byte* pDataB)
+        {
+            EnsurePixel();
+            if (Pixel.A == byte.MaxValue)
+            {
+                *pDataA = byte.MaxValue;
+                *pDataR = Pixel.R;
+                *pDataG = Pixel.G;
+                *pDataB = Pixel.B;
+            }
+            else
+            {
+                PixelHelper.Overlay(ref pDataA, ref pDataR, ref pDataG, ref pDataB, Pixel.A, Pixel.R, Pixel.G, Pixel.B);
+            }
+        }
+
+        private bool IsPixelValid = false;
+        private void EnsurePixel()
+        {
+            if (IsPixelValid)
+                return;
+
+            Pixel.Override(Adapter.A, Adapter.R, Adapter.G, Adapter.B);
+            IsPixelValid = true;
+        }
+        private byte Adjust(byte Value)
+        {
+            if (Cache.TryGetValue(Value, out byte Result))
+                return Result;
+
+            Result = (byte)(Math.Pow(Value / 255.0, Gamma) * 255.0);
+            Cache[Value] = Result;
+            return Result;
+        }
+
+        public override void DangerousMove(int X, int Y)
+        {
+            Adapter.DangerousMove(X, Y);
+            IsPixelValid = false;
+        }
+        public override void DangerousOffsetX(int OffsetX)
+        {
+            Adapter.DangerousOffsetX(OffsetX);
+            IsPixelValid = false;
+        }
+        public override void DangerousOffsetY(int OffsetY)
+        {
+            Adapter.DangerousOffsetY(OffsetY);
+            IsPixelValid = false;
+        }
+
+        public override void DangerousMoveNextX()
+        {
+            Adapter.DangerousMoveNextX();
+            IsPixelValid = false;
+        }
+        public override void DangerousMovePreviousX()
+        {
+            Adapter.DangerousMovePreviousX();
+            IsPixelValid = false;
+        }
+
+        public override void DangerousMoveNextY()
+        {
+            Adapter.DangerousMoveNextY();
+            IsPixelValid = false;
+        }
+        public override void DangerousMovePreviousY()
+        {
+            Adapter.DangerousMovePreviousY();
+            IsPixelValid = false;
+        }
+
+        public override PixelAdapter<T> Clone()
+            => new AdjustGammaPixelAdapter<T>(this);
+
+    }
+}
